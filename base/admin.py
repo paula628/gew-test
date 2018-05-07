cf478cc7523ffa45345bcc28b5dbfa8c0c94bc8a
paula628 <paulatanada628@gmail.com>
2018-05-07 16:16:32 +0200

    add Consumer model in admin

--- base/admin.py
+++ base/admin.py
@@ -3,9 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Question, TempUser, Answer
+from .models import Question, TempUser, Answer, Consumer
 
 admin.site.register(Question)
 admin.site.register(TempUser)
 admin.site.register(Answer)
+admin.site.register(Consumer)
 
