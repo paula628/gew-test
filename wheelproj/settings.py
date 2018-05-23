d6eba0805528c6181c558d60367f10908849ac08
paula628 <paulatanada628@gmail.com>
2018-05-23 15:09:48 +0000

    changed db settings for mysql

--- wheelproj/settings.py
+++ wheelproj/settings.py
@@ -87,13 +87,16 @@ WSGI_APPLICATION = 'wheelproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'escpdigital$default',
+        'USER': 'escpdigital',
+        'PASSWORD': 'escpescp',
+	    'HOST' : 'escpdigital.mysql.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
