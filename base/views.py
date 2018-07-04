70c9e50c2abd73befc17c90dea2cc84f0b9b9b11
paula628 <paulatanada628@gmail.com>
2018-07-04 12:34:24 +0200

    remove frequency graph from tag graphs page

--- base/views.py
+++ base/views.py
@@ -455,7 +455,7 @@ def answers_by_tag_graph(request, tag=False):
                 'emotion_colors' : json.dumps(emotion_colors),
                 'colors': json.dumps(colors),
                 })
-    return render(request, 'base/answers_graph.html', context)
+    return render(request, 'base/answers_graph_by_student_tag.html', context)
 
 
 
@@ -542,7 +542,7 @@ def answers_by_student_graph(request, student_id=None):
                 'emotion_colors' : json.dumps(emotion_colors),
                 'colors': json.dumps(colors),
                 })
-    return render(request, 'base/answers_graph_by_student.html', context)
+    return render(request, 'base/answers_graph_by_student_tag.html', context)
 
 
 def paginate(page, object_list, count_per_page=10):
